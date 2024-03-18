d2ecd81c696a361352328073eb0d60d33ccac34f
Jordan Baron <jordannbaron@gmail.com>
2024-03-17 22:06:12 -0400

    setup

---
+++ src/main.zig
@@ -0,0 +1,24 @@
+// raylib-zig (c) Nikolas Wipper 2023
+
+const rl = @import("raylib");
+
+pub fn main() anyerror!void {
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    defer rl.closeWindow(); // Close window and OpenGL context
+
+    rl.setTargetFPS(60);
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(rl.Color.black);
+
+        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
+    }
+}
---
+++ zig-cache/o/cb03f7783df4dca409d80cabe5b861a9/dependencies.zig
@@ -0,0 +1,6 @@
+pub const imports = struct {
+    pub const raylib = @import("1220c28847ca8e8756734ae84355802b764c9d9cf4de057dbc6fc2b15c56e726f27b");
+};
+pub const build_root = struct {
+    pub const raylib = "/root/.cache/zig/p/1220c28847ca8e8756734ae84355802b764c9d9cf4de057dbc6fc2b15c56e726f27b";
+};
